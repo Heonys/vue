b6e7313e3febeb05d71c213e8ff3bf61abab37db
heon <siwmua0@naver.com>
2022-03-09 07:11:58 +0900

    todo it

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="inputBox shadow">
+      <input type="text" v-model="todoData" @keyup.enter="addTodo">
+
+      <span class="addContainer" @click="addTodo">
+        <i class="fa-solid fa-plus addBtn"></i>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoData: "",
+    }
+  },
+  methods: {
+    addTodo(){
+      localStorage.setItem(this.todoData, this.todoData);
+      this.clearInput()
+    },
+    clearInput(){
+      this.todoData = "";
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+input:focus{
+  outline: none;
+}
+
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input{
+  border-style: none;
+  font-size: 0.9rem;
+}
+
+.addContainer{
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+
+
+
+
+
+</style>
