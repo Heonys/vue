c6abceab065839fff18f33d0ca6da59eead69549
heon <siwmua0@naver.com>
2022-06-25 22:03:03 +0900

    end-gmae

---
+++ practice/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>test</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    created() {
+        console.log(process.env.VUE_APP_TITLE);
+    },
+};
+</script>
+
+<style></style>
